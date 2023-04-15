cbb3e288a8504ac51ed3c673d2274c8a015b6978
Luiscaroca <kako.ljca@gmail.com>
2023-04-14 20:49:53 -0400

    Eliminadas dependencias no utilizadas, agregadas dependencias para trabajar backend y nodemailer

--- src/main.js
+++ src/main.js
@@ -3,29 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-import { faStop } from "@fortawesome/free-solid-svg-icons";
-
-library.add(
-  faSquareFacebook,
-  faInstagram,
-  faEnvelope,
-  faWhatsapp,
-  faCopyright,
-  faStop
-);
 
 createApp(App)
   .use(router)
   .use(BootstrapIconsPlugin)
-  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
